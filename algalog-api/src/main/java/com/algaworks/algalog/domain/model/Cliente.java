b3a0bedd117049b72f1901784700aa3907c1df03
AndréLucasSilva <andlucsil1221@gmail.com>
2022-11-03 11:36:41 -0300

    added BeanValidation

--- algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -7,6 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -24,12 +27,19 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(max = 60)
 	@Column(name="nome")
 	private String nome;
 	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	@Column(name="email")
 	private String email;
 	
+	@NotBlank
+	@Size(max = 20)
 	@Column(name="fone")
 	private String telefone;
 	
